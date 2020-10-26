68d6a9a02d09e454f742b3338925e9ac3a3bcea2
hayukimori <69482956+hayukimori@users.noreply.github.com>
2020-10-25 21:15:41 -0300

    Add files via upload

---
+++ commandList.py
@@ -0,0 +1,25 @@
+import os
+import time
+os.system("clear")
+
+print ('\033[31m'+'Comandos System Call'+'\033[0;0m')
+
+print('\033[32m'+'\n #CARDINAL\n '+'\033[0;0m')
+
+print('\033[36m'+'Generate class'+'\033[0;0m')
+
+print("\ngenerate umbra element")
+
+print("\ngenrate luminous element")
+
+print("\njikan")
+
+print("\nLINK START")
+
+print("\nSTACIA")
+
+print("\nopen note")
+
+print("\n\n")
+
+os.system("sh")
---
+++ stacia.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+print("\n\nSTACIA WINDOW v 1.0\n\n")
+
+time.sleep(2)
+
+os.system("clear")
+
+print("\n\n\n\n\n\n\n\n\n\n\n HAYUKIMORI \n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+time.sleep(0.5)
+
+os.system("clear")
+
+print("-------------------------------")
+
+os.system("date")
+
+print("\n username :")
+os.system("who")
+print("\n")
+
+print("-------------------------------")
+
+time.sleep(10)
+
