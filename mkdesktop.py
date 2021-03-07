7821e394604ba51513b49303e8ee3cf6f17d840f
Redson dos Santos Silva <61910521+RedsonBr140@users.noreply.github.com>
2021-03-07 18:35:38 -0300

    Mkdesktop modificado.

--- mkdesktop.py
+++ mkdesktop.py
@@ -8,7 +8,7 @@ home = user.replace('\n','')
 
 
 
-arquivo = "[Desktop Entry]\nVersion=1.0\nName=System Call\nExec=sh " + home + "/startscapp.sh \nTerminal=false\nType=Application\nStartupNotify=true"
+arquivo = "[Desktop Entry]\nVersion=1.0\nName=System Call\nExec=xterm -e python3 "" + home + ".local/share/SystemCallApp/main.py" \nTerminal=false\nType=Application\nStartupNotify=true"
 
 os.system('echo "' + arquivo + '" > systemcall-py.desktop')
 
@@ -16,9 +16,3 @@ tmp.close()
 os.system("rm userhome")
 
 print("feito")
-
-
-
-
-
-
